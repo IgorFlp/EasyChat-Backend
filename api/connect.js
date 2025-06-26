43432ee7c6b6eb20981eff74b13bf355f2945b29
IgorFlp <78287584+IgorFlp@users.noreply.github.com>
2025-06-26 17:52:31 -0300

    Changed from Mongo to DSQL DB, get and post MessageDB adapted

--- api/connect.js
+++ api/connect.js
@@ -1,9 +1,46 @@
-import { MongoClient } from "mongodb";
+import { DsqlSigner } from "@aws-sdk/dsql-signer";
+import postgres from "postgres";
+import assert from "node:assert";
 import "./loadEnv.js";
 
-const user = process.env.MONGO_USER;
-const password = process.env.MONGO_PASSWORD;
-const URI = `mongodb+srv://${user}:${password}@cluster0.shq1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const ADMIN = "admin";
+const PUBLIC = "public";
+const SCHEMA = "EasyChat";
 
-const client = new MongoClient(URI);
-export const db = client.db("EasyChat");
+export async function getConnection() {
+  const clusterEndpoint = process.env.CLUSTER_ENDPOINT;
+  assert(clusterEndpoint);
+  const user = process.env.CLUSTER_USER;
+  assert(user);
+  const region = process.env.CLUSTER_REGION;
+  assert(region);
+  let client = postgres({
+    host: clusterEndpoint,
+    user: user,
+    // We can pass a function to password instead of a value, which will be triggered whenever
+    // connections are opened.
+    password: async () => await getPasswordToken(clusterEndpoint, user, region),
+    database: "postgres",
+    port: 5432,
+    idle_timeout: 2,
+    ssl: {
+      rejectUnauthorized: true,
+    },
+    // max: 1, // Optionally set maximum connection pool size
+  });
+
+  return client;
+}
+
+async function getPasswordToken(clusterEndpoint, user, region) {
+  const signer = new DsqlSigner({
+    hostname: clusterEndpoint,
+    region,
+  });
+  if (user === ADMIN) {
+    return await signer.getDbConnectAdminAuthToken();
+  } else {
+    signer.user = user;
+    return await signer.getDbConnectAuthToken();
+  }
+}
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import axios from "axios";
+//import { GetMessages } from "./mensagensDAO.js";
+import { getConnection } from "./connect.js";
 import "./loadEnv.js";
 import { router } from "../routes/webhook.js";
-import { db } from "./connect.js";
 import cors from "cors";
 
+const SCHEMA = "EasyChat";
 const WPP_MY_NUMBER_ID = process.env.WPP_MY_NUMBER_ID;
 const app = express();
 app.use(express.json());
@@ -26,91 +28,68 @@ app.get("/status", (request, response) => {
 });
 
 app.get("/messagesDB", async (request, response) => {
-  const received = await db
-    .collection("Received_Messages")
-    .find({})
-    .toArray()
-    .catch((error) => {
-      console.error("Erro ao buscar mensagens:", error);
-      response.status(500).send("Erro ao buscar mensagens");
-    });
-  const sent = await db
-    .collection("Sent_Messages")
-    .find({})
-    .toArray()
-    .catch((error) => {
-      console.error("Erro ao buscar mensagens:", error);
-      response.status(500).send("Erro ao buscar mensagens");
-    });
-  if (!received || !sent) {
-    response.status(404).send("Mensagem não encontrada");
-  } else {
-    response.status(200).send({ received, sent });
-  }
+  let client = await getConnection();
+  const messages = await client`SELECT * FROM ${client(SCHEMA)}.mensagens;`;
+  response.send(messages);
 });
 
-app.get("/messagesDB/:number", async (request, response) => {
-  const { number } = request.params;
+app.get("/messagesDB/:contact", async (request, response) => {
+  const { contact } = request.params;
+  let client = await getConnection();
   //msgRecieved "recipient_id": "5518998200826"
   //msgSent "to": "5518998200826"
-  const received = await db
-    .collection("Received_Messages")
-    .find({ "entry.changes.value.messages.from": number })
-    .toArray()
-    .catch((error) => {
-      console.error("Erro ao buscar mensagem:", error);
-      response.status(500).send("Erro ao buscar mensagem");
-    });
-  const sent = await db
-    .collection("Sent_Messages")
-    .find({ to: number })
-    .toArray()
-    .catch((error) => {
-      console.error("Erro ao buscar mensagem:", error);
-      response.status(500).send("Erro ao buscar mensagem");
-    });
-  if (!received || !sent) {
-    response.status(404).send("Mensagem não encontrada");
-  } else {
-    response.status(200).send({ received, sent });
+  let messages = await client`SELECT * FROM ${client(
+    SCHEMA
+  )}.mensagens WHERE recipient = ${contact} OR sender = ${contact};`;
+  if (messages.length === 0) {
+    response
+      .status(404)
+      .send("Nenhuma mensagem encontrada para este número ou perfil.");
+    return;
   }
+  response.send(messages);
 });
 
 app.post("/messageDB/:mode", async (request, response) => {
   const { mode } = request.params;
-  if (mode == "sent") {
-    const message = request.body;
-    message.mode = "sent";
-    console.log(message);
-    const sent = await db
-      .collection("Sent_Messages")
-      .insertOne(message)
-      .catch((error) => {
-        console.error("Erro ao inserir mensagem:", error);
-        response.status(500).send("Erro ao inserir mensagem");
-      });
-    if (!sent) {
-      response.status(404).send("Mensagem não encontrada");
-    } else {
-      response.status(200).send(sent);
-    }
+  const message = request.body;
+  let recepient = mode === "received" ? process.env.WPP_MY_NUMBER : message.to;
+  console.log(
+    "Received Message:",
+    message.entry[0].changes[0].value.messages[0]
+  );
+  let formatedMessage = {};
+  if (message.entry[0]) {
+    formatedMessage = {
+      id: message.entry[0].id,
+      recipient: recepient,
+      sender: message.entry[0].changes[0].value.messages[0].from,
+      mode: mode,
+      type: message.entry[0].changes[0].value.messages[0].type,
+      text: message.entry[0].changes[0].value.messages[0].text.body,
+      arquivo_url: "",
+      timestamp: message.entry[0].changes[0].value.messages[0].timestamp,
+      metadados: message.entry[0].changes[0].value.metadata,
+      source: message.entry[0].changes[0].value.messaging_product,
+    };
   }
-  if (mode == "received") {
-    const message = request.body;
-    message.mode = "received";
-    console.log(message);
-    const sent = await db
-      .collection("Received_Messages")
-      .insertOne(message)
-      .catch((error) => {
-        console.error("Erro ao inserir mensagem:", error);
-        response.status(500).send("Erro ao inserir mensagem");
-      });
-    if (!sent) {
-      response.status(404).send("Mensagem não encontrada");
-    } else {
-      response.status(200).send(sent);
-    }
+  console.log("Formated Message:", formatedMessage);
+
+  let client = await getConnection();
+  const res = await client`INSERT INTO ${client(SCHEMA)}.mensagens
+    (id, recipient, sender, "mode", "type", text, arquivo_url, "timestamp", metadados, "source")
+    VALUES(${formatedMessage.id}, ${formatedMessage.recipient}, ${
+    formatedMessage.sender
+  }, ${formatedMessage.mode}, ${formatedMessage.type}, ${
+    formatedMessage.text
+  }, ${formatedMessage.arquivo_url}, ${formatedMessage.timestamp}, ${
+    formatedMessage.metadados
+  }, ${formatedMessage.source});`;
+
+  if (!res) {
+    response.status(404).send("Mensagem não encontrada");
+  } else {
+    response.status(200).send(sent);
   }
 });
 
