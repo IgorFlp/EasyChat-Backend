06a9c1918cefea405d3fec0536cf6a56f28fb6e7
IgorFlp <igor.felipe.13@hotmail.com>
2025-07-23 17:31:53 -0300

    Migration Aurora DSQL to RDS Postgres

--- api/connect.js
+++ api/connect.js
@@ -1,46 +1,21 @@
-import { DsqlSigner } from "@aws-sdk/dsql-signer";
-import postgres from "postgres";
-import assert from "node:assert";
+import { Client }  from 'pg';
 import "./loadEnv.js";
 
-const ADMIN = "admin";
-const PUBLIC = "public";
-const SCHEMA = "EasyChat";
+  export async function getConnection() {
+  const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-export async function getConnection() {
-  
-  const clusterEndpoint = process.env.CLUSTER_ENDPOINT;
-  assert(clusterEndpoint);
-  const user = process.env.CLUSTER_USER;
-  assert(user);
-  const region = process.env.CLUSTER_REGION;
-  assert(region);
-
-  let client = postgres({
-    host: clusterEndpoint,
-    user: user,
-    password: async () => await getPasswordToken(clusterEndpoint, user, region),
-    database: "postgres",
-    port: 5432,
-    idle_timeout: 2,
-    ssl: {
-      rejectUnauthorized: true,
-    },
-    // max: 1, // Optionally set maximum connection pool size
-  });
+client.connect()
+  .then(() => console.log('Conectado com SSL!'))
+  .catch(e => console.error(e));
 
   return client;
-}
-
-async function getPasswordToken(clusterEndpoint, user, region) {
-  const signer = new DsqlSigner({
-    hostname: clusterEndpoint,
-    region,
-  });
-  if (user === ADMIN) {
-    return await signer.getDbConnectAdminAuthToken();
-  } else {
-    signer.user = user;
-    return await signer.getDbConnectAuthToken();
-  }
 }
