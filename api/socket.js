4fc1c366c2cb2c79c10573426b0d374019066167
IgorFlp <igor.felipe.13@hotmail.com>
2026-01-27 16:26:21 -0300

    Socket cors fix

--- api/socket.js
+++ api/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CORS_ORIGIN,
+    origin: ["http://localhost:5173", process.env.CORS_ORIGIN],
     methods: ["GET", "POST"],
     credentials: true,
   },
