96be009c14fcb5a5f5278ef7a43eee136f260d78
IgorFlp <igor.felipe.13@hotmail.com>
2026-01-28 09:52:50 -0300

    Localhost cors rule

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["http://localhost:5173", process.env.CORS_ORIGIN],
     credentials: true,
   })
 );
